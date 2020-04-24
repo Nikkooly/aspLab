567983dbd5d805436c3d466f534708946db7c35a
Nikkooly <acaroshen@gmail.com>
2020-04-24 12:31:14 +0300

    release

---
+++ lab3/lab3/Controllers/DictController.cs
@@ -0,0 +1,70 @@
+﻿using lab3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab3.Controllers
+{
+    public class DictController : Controller
+    {
+        // GET: Dict
+        PhoneBookContext phoneBookContext;
+        public ActionResult Index()
+        {
+            phoneBookContext = new PhoneBookContext(Server.MapPath("~/Models/Data.json"));
+            ViewBag.PhoneBooks = phoneBookContext.PhoneBooks;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddSave(string surname, string telephone)
+        {
+            phoneBookContext = new PhoneBookContext(Server.MapPath("~/Models/Data.json"));
+            phoneBookContext.Add(surname, telephone);
+            ViewBag.PhoneBooks = phoneBookContext.PhoneBooks;
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Update(string idSelectedItem)
+        {
+            ViewBag.Id = idSelectedItem;
+            phoneBookContext = new PhoneBookContext(Server.MapPath("~/Models/Data.json"));
+            ViewBag.PhoneBooks = phoneBookContext.PhoneBooks;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateSave(string id, string surname, string telephone)
+        {
+            phoneBookContext = new PhoneBookContext(Server.MapPath("~/Models/Data.json"));
+            phoneBookContext.Update(Guid.Parse(id), surname, telephone);
+            ViewBag.PhoneBooks = phoneBookContext.PhoneBooks;
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(string delSelectedItem)
+        {
+            ViewBag.Id = delSelectedItem;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteSave(string id)
+        {
+            phoneBookContext = new PhoneBookContext(Server.MapPath("~/Models/Data.json"));
+            phoneBookContext.Delete(Guid.Parse(id));
+            ViewBag.PhoneBooks = phoneBookContext.PhoneBooks;
+            return View("Index");
+        }
+    }
+}
---
+++ lab3/lab3/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab3.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            ViewBag.url = Request.RawUrl;
+            ViewBag.method = Request.HttpMethod;
+            return View();
+        }
+    }
+}
