a2cc11977e575423194ea98c5053761bfb2c41e3
Nikkooly <acaroshen@gmail.com>
2020-05-21 09:01:22 +0300

    release

---
+++ lab_5a2/WebApplication/Controllers/CResearchController.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class CResearchController : Controller
+    {
+        [AcceptVerbs("GET", "POST")]
+        public ActionResult C01()
+        {
+            ViewBag.Rd = this.RouteData.Values;
+
+            return View();
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        public ActionResult C02()
+        {
+            var rd = this.RouteData.Values;
+            var response = new StringBuilder();
+            foreach (string key in Request.Headers)
+            {
+                var value = Request.Headers[key];
+                response.AppendLine(value + "<br />");
+            }
+            response.AppendLine("RawURI - " + Request.RawUrl + "<br />");
+            response.AppendLine("Status - " + Response.Status + "<br />");
+            response.AppendLine("Body (form) - " + Request.Form + "<br />");
+            Response.Write(response.ToString());
+            ViewBag.Rd = rd;
+            return View();
+        }
+    }
+}
